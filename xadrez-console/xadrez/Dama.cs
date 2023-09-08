7bad1104d02cffc9da0680b0259a3f750cec007b
OQueLucas <lucasestevam365@gmail.com>
2023-09-08 11:14:36 -0300

    Acrescentando as demais pecas

--- xadrez-console/xadrez/Dama.cs
+++ xadrez-console/xadrez/Dama.cs
@@ -2,14 +2,14 @@ using tabuleiro;
 
 namespace xadrez
 {
-    class Bispo : Peca
+    class Dama : Peca
     {
-        public Bispo(Tabuleiro tab, Cor cor)
+        public Dama(Tabuleiro tab, Cor cor)
             : base(tab, cor) { }
 
         public override string ToString()
         {
-            return "B";
+            return "D";
         }
 
         private bool podeMover(Posicao pos)
@@ -23,6 +23,54 @@ namespace xadrez
             bool[,] mat = new bool[tab.linhas, tab.colunas];
 
             Posicao pos = new Posicao(0, 0);
+
+            // norte
+            pos.definirValores(posicao.linha - 1, posicao.coluna);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.linha = pos.linha - 1;
+            }
+
+            // sul
+            pos.definirValores(posicao.linha + 1, posicao.coluna);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.linha = pos.linha + 1;
+            }
+
+            // leste
+            pos.definirValores(posicao.linha, posicao.coluna + 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna + 1;
+            }
+
+            // oeste
+            pos.definirValores(posicao.linha, posicao.coluna - 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna - 1;
+            }
 
             // noroeste
             pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
